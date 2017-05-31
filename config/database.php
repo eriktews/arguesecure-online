4ec4958bcab9ee093d272d6497c5268d19a660ae
Erik Tews <erik@datenzone.de>
2017-05-31 19:23:39 +0200

    Control the redis host via REDIS_HOST.

--- config/database.php
+++ config/database.php
@@ -116,7 +116,7 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => '127.0.0.1',
+            'host'     => env('REDIS_HOST', 'redis'),
             'port'     => 6379,
             'database' => 0,
         ],
@@ -124,3 +124,4 @@ return [
     ],
 
 ];
+
